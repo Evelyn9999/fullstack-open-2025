e866d0ae7641eabbf4e23f848c2ee9d9ef762ca6
Evelyn <sapphiry0404@gmail.com>
2025-09-10 14:50:55 +0300

    ex1,2 anser done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,9 +1,13 @@
 const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({name, exercise}) => (
+    <p>{name} {exercise}</p>
+)
 const Content = ({part1, exercise1, part2, exercise2, part3, exercise3}) => (
     <div>
-        <p>{part1} {exercise1}</p>
-        <p>{part2} {exercise2}</p>
-        <p>{part3} {exercise3}</p>
+        <Part name={part1} exercise={exercise1}/>
+        <Part name={part2} exercise={exercise2}/>
+        <Part name={part3} exercise={exercise3}/>
     </div>
 )
 
