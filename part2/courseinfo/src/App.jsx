a5817fbc3a7816d2de9a803797de01d345969c9f
Evelyn <sapphiry0404@gmail.com>
2025-09-12 21:55:53 +0300

    part2 ex2,1-ex2,2 added

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,64 @@
+const Header = ({name}) => <h1>{name}</h1>
+
+const Part = ({part}) => (
+    <p>
+        {part.name} {part.exercises}
+    </p>
+)
+const Content = ({parts}) => (
+    <div>
+        {parts.map((p) => (
+            <Part key={p.id} part={p} />
+        ))}
+    </div>
+)
+
+const Total = ({parts}) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+        <p><strong>total of {total} exercises</strong></p>
+    )
+}
+
+// Course Comb combines Header + Content
+const Course = ({ course }) => (
+    <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts}/>
+    </div>
+)
+
+
+const App = () => {
+    const course = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
+            },
+        ],
+    }
+
+    return <Course course={course} />
+}
+
+export default App
