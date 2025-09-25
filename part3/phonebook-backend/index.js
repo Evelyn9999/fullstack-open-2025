70fd34917480670e5cd68c5db0e22d75c7e7f5be
Evelyn <sapphiry0404@gmail.com>
2025-09-25 13:49:04 +0300

    ex2,4 delete added

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,30 +5,34 @@ app.use(express.json()) // JSON body parser
 
 let persons =[
     {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        id: "1",
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        id: "2",
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+        id: "3",
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        id: "4",
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
     ]
 
 // STEP 3.1
 app.get('/', (req, res) => {
     res.send('<h1>Phonebook</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
 // STEP 3,2 info page
@@ -41,11 +45,26 @@ app.get('/info', (req, res) => {
     `)
 })
 
+// STEP 3,3 single note
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    console.log('looking for id:', id, 'in', persons.map(p => p.id))
+    const p = persons.find(p => p.id === id)
 
-
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    if (p) {
+        res.json(p)
+    } else {
+        return res.status(404).end()
+    }
 })
+
+// STEP 3.4 delete
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
